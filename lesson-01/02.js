a2a6ae6161c6f3c72daa92fe9f0b592a8561dfd7
Dzmitry <hunter250185@gmail.com>
2023-10-30 20:29:34 +0300

    test10

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,6 +1,3 @@
 let number = Math.floor(Math.random() * 100);
 console.log(`Сгенерировано число: ${number},
  ${number % 2 === 0 ? 'четное' : 'нечетное'}`);
- 
- let number1 = Math.floor(Math.random() * 100);
- console.log(number1 + (number1 % 2 === 0 ? " : Это четное число" : " : Это нечетное число"));

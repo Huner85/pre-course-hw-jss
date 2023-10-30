6c749e35ace55e10b018afbbffe43d58ebaf06a9
Dzmitry <hunter250185@gmail.com>
2023-10-30 21:32:31 +0300

    text

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,3 +1,9 @@
 let number = Math.floor(Math.random() * 100);
 console.log(`Сгенерировано число: ${number},
  ${number % 2 === 0 ? 'четное' : 'нечетное'}`);
+
+ let number1 = Math.floor(Math.random() * 100);
+if (number1 % 2 === 0) {
+    console.log(number1, ': четное');
+} else {
+    console.log(number1, ': нечетное');

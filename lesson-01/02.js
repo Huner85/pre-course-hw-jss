8c5d1b68ffdc61bcaf7d84f98e90d699e389784d
Dzmitry <hunter250185@gmail.com>
2023-10-31 19:34:05 +0300

    fix3

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,8 +1,2 @@
 let number1 = Math.floor(Math.random() * 100);
 console.log(number1 + (number1 % 2 === 0 ? " : Это четное число" : " : Это нечетное число"));
-
-let number = 1;
-console.log(number);
-console.log(++number);
-console.log(--number);
-console.log(number++)

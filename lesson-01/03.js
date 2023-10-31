d04e97d4c9d2035e194ee1873270b4305302d027
Dzmitry <hunter250185@gmail.com>
2023-10-31 21:19:19 +0300

    fix15

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -1,9 +1,7 @@
 let number = 1;
-number++
-console.log(--number);
-number++;
-console.log(++number);
-number++
-console.log(--number); 
-number--;
-console.log(--number); 
+console.log(number);
+number = ++number + 1;
+console.log(number);
+console.log(number); 
+number = --number - 1;
+console.log(number);
